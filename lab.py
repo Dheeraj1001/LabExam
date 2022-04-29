efd411cc1d394f562aa4122579c89e7e03def951
Dheeraj1001 <55041526+Dheeraj1001@users.noreply.github.com>
2022-04-29 09:39:56 +0530

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,1 +1,13 @@
-print("Dheeraj")
+print("Dheeraj Theegela")
+print("E19CSE393 \n")
+int no = int(input("Enter a Number: "))
+if no > 1:
+    for i in range(2, int(no/2)+1):
+        if (no % i) == 0:
+            print(no, "is not a prime number")
+            break
+    else:
+        print(no, "is a prime number")
+  
+else:
+    print(no, "is not a prime number")
